65d7fc8910dbd48d227645e632a0d3af2c5e81de
Nickock <=>
2025-09-11 22:20:29 -0300

    finalizado la introducción a recorridas de grafos, y actualizacion en el footer, para link de github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { FaDev } from "react-icons/fa";
 export function Footer(){
     return(
         <footer className="main-footer">
-            <a href=""><FaDev/></a>
-            <a href=""><BsGithub/></a>
+            {/* <a href=""><FaDev/></a> */}
+            <a href="https://github.com/Nickock/code-dojo"><BsGithub/></a>
             
         </footer>
     )
